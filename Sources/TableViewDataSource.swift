321b45694a069c8d1c0068095bbab4fd1250ec29
Prince Ugwuh <pugwuh@inmotionsoftware.com>
2016-09-14 13:07:07 -0500

    swift 3.0 support

--- Sources/TableViewDataSource.swift
+++ Sources/TableViewDataSource.swift
@@ -3,38 +3,38 @@ import UIKit
 @objc(MSTableViewCellDataSource)
 public protocol TableViewCellDataSource: class {
     @objc(configureDataForCell:)
-    func configure(data: AnyObject?)
+    func configure(_ data: AnyObject?)
 }
 
 @objc(MSTableViewDataSourceDelegate)
 public protocol TableViewDataSourceDelegate: class {
     @objc(dataSource:didConfigureCellAtIndexPath:)
-    func didConfigureCell(cell: TableViewCellDataSource, atIndexPath indexPath: NSIndexPath)
+    func didConfigureCell(_ cell: TableViewCellDataSource, atIndexPath indexPath: IndexPath)
     
     @objc(dataSource:commitEditingStyleForIndexPath:)
-    optional func commitEditingStyle(editingStyle: UITableViewCellEditingStyle, forIndexPath indexPath: NSIndexPath)
+    optional func commitEditingStyle(_ editingStyle: UITableViewCellEditingStyle, forIndexPath indexPath: IndexPath)
 }
 
 @objc(MSTableViewDataSourceCellItem)
-public class TableViewDataSourceCellItem: NSObject {
+open class TableViewDataSourceCellItem: NSObject {
     var cellIdentifier: String?
     var item: AnyObject?
     var cellHeight: CGFloat?
 }
 
 @objc(MSTableViewDataSource)
-public class TableViewDataSource: NSObject, UITableViewDataSource {
-    private var cellItems: [TableViewDataSourceCellItem]?
-    private(set) var cellIdentifier: String?
-    public var items: [AnyObject]?
-    public var title: String?
-    public weak var delegate: TableViewDataSourceDelegate?
-    public var sectionHeaderView: UIView?
-    public var reusableHeaderFooterViewIdentifier: String?
-    public var editable: Bool = false
-    public var movable: Bool = false
-    public var editableCells: [NSIndexPath: NSNumber]? // NSNumber represents the UITableViewCellEditingStyle
-    public var movableCells: [NSIndexPath]?
+open class TableViewDataSource: NSObject, UITableViewDataSource {
+    internal var cellItems: [TableViewDataSourceCellItem]?
+    internal(set) var cellIdentifier: String?
+    open var items: [AnyObject]?
+    open var title: String?
+    open weak var delegate: TableViewDataSourceDelegate?
+    open var sectionHeaderView: UIView?
+    open var reusableHeaderFooterViewIdentifier: String?
+    open var editable: Bool = false
+    open var movable: Bool = false
+    open var editableCells: [IndexPath: NSNumber]? // NSNumber represents the UITableViewCellEditingStyle
+    open var movableCells: [IndexPath]?
     
     override init() {
         super.init()
@@ -54,16 +54,16 @@ public class TableViewDataSource: NSObject, UITableViewDataSource {
     //MARK: -
     
     @objc(itemAtIndexPath:)
-    public func item(at indexPath: NSIndexPath) -> AnyObject? {
-        if let items = items where items.count > 0 && indexPath.row < items.count {
+    open func item(at indexPath: IndexPath) -> AnyObject? {
+        if let items = self.items , items.count > 0 && indexPath.row < items.count {
             return items[indexPath.row]
         }
         return nil
     }
     
     @objc(cellItemAtIndexPath:)
-    public func cellItem(at indexPath: NSIndexPath) -> TableViewDataSourceCellItem? {
-        if let cellItems = cellItems where cellItems.count > 0 && indexPath.row < cellItems.count {
+    open func cellItem(at indexPath: IndexPath) -> TableViewDataSourceCellItem? {
+        if let cellItems = self.cellItems , cellItems.count > 0 && indexPath.row < cellItems.count {
             return cellItems[indexPath.row]
         }
         return nil
@@ -71,23 +71,23 @@ public class TableViewDataSource: NSObject, UITableViewDataSource {
     
     //MARK: - UITableViewDataSource
     
-    public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    open func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let cellItems = cellItems {
+    open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let cellItems = self.cellItems {
             return cellItems.count
         }
         
-        if let items = items {
+        if let items = self.items {
             return items.count
         }
         return 0
     }
     
-    public func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        if movable {
+    open func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        if self.movable {
             if let movableCells = movableCells {
                 return movableCells.contains(indexPath)
             }
@@ -95,8 +95,8 @@ public class TableViewDataSource: NSObject, UITableViewDataSource {
         return false
     }
     
-    public func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        if editable {
+    open func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if self.editable {
             if let editableCells = editableCells {
                 return editableCells.keys.contains(indexPath)
             }
@@ -104,23 +104,23 @@ public class TableViewDataSource: NSObject, UITableViewDataSource {
         return false
     }
     
-    public func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        delegate?.commitEditingStyle?(editingStyle, forIndexPath: indexPath)
+    open func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        self.delegate?.commitEditingStyle?(editingStyle, forIndexPath: indexPath)
     }
     
-    public func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+    open func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         if let item = items?[sourceIndexPath.row] {
-            items?.removeAtIndex(sourceIndexPath.row)
-            items?.insert(item, atIndex: destinationIndexPath.row)
+            self.items?.remove(at: sourceIndexPath.row)
+            self.items?.insert(item, at: destinationIndexPath.row)
         }
     }
     
     
-    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var identifier: String?
         var item: AnyObject?
         
-        if let cellItems = cellItems where cellItems.count > 0 {
+        if let cellItems = cellItems , cellItems.count > 0 {
             let cellItem = self.cellItem(at: indexPath)!
             identifier = cellItem.cellIdentifier
             item = cellItem.item
@@ -129,7 +129,7 @@ public class TableViewDataSource: NSObject, UITableViewDataSource {
             identifier = cellIdentifier
         }
         
-        let cell = tableView.dequeueReusableCellWithIdentifier(identifier!, forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier!, for: indexPath)
         
         if cell is TableViewCellDataSource {
             let sourceCell = cell as! TableViewCellDataSource
@@ -142,12 +142,12 @@ public class TableViewDataSource: NSObject, UITableViewDataSource {
 }
 
 @objc(MSTableViewSectionDataSource)
-public class TableViewSectionDataSource: NSObject, UITableViewDataSource {
+open class TableViewSectionDataSource: NSObject, UITableViewDataSource {
     
-    public weak var delegate: TableViewDataSourceDelegate?
-    public private(set) var dataSources: [TableViewDataSource]?
-    public var enableIndexing: Bool  = false
-    public var showDefaultHeaderTitle: Bool = true
+    open weak var delegate: TableViewDataSourceDelegate?
+    open fileprivate(set) var dataSources: [TableViewDataSource]?
+    open var enableIndexing: Bool  = false
+    open var showDefaultHeaderTitle: Bool = true
     
     public convenience init(dataSources: [TableViewDataSource]) {
         self.init()
@@ -155,97 +155,96 @@ public class TableViewSectionDataSource: NSObject, UITableViewDataSource {
     }
     
     @objc(itemAtIndexPath:)
-    public func item(at indexPath: NSIndexPath) -> AnyObject? {
-        let dataSource = dataSources![indexPath.section]
+    open func item(at indexPath: IndexPath) -> AnyObject? {
+        let dataSource = self.dataSources![indexPath.section]
         return dataSource.item(at: indexPath)
     }
     
     @objc(cellItemAtIndexPath:)
-    public func cellItem(at indexPath: NSIndexPath) -> TableViewDataSourceCellItem? {
-        let dataSource = dataSources![indexPath.section]
+    open func cellItem(at indexPath: IndexPath) -> TableViewDataSourceCellItem? {
+        let dataSource = self.dataSources![indexPath.section]
         return dataSource.cellItem(at: indexPath)
     }
     
     @objc(dataSourceAtIndexPath:)
-    public func dataSource(at indexPath: NSIndexPath) -> TableViewDataSource {
-        return dataSources![indexPath.section]
+    open func dataSource(at indexPath: IndexPath) -> TableViewDataSource {
+        return self.dataSources![indexPath.section]
     }
     
     @objc(titleAtIndexPath:)
-    public func title(at indexPath: NSIndexPath) -> String? {
-        let dataSource = dataSources![indexPath.section]
+    open func title(at indexPath: IndexPath) -> String? {
+        let dataSource = self.dataSources![indexPath.section]
         return dataSource.title
     }
     
     @objc(sectionHeaderViewAtIndexPath:)
-    public func sectionHeaderView(at indexPath: NSIndexPath) -> UIView? {
-        let dataSource = dataSources![indexPath.section]
+    open func sectionHeaderView(at indexPath: IndexPath) -> UIView? {
+        let dataSource = self.dataSources![indexPath.section]
         return dataSource.sectionHeaderView
     }
     
     //MARK: - UITableViewDataSource
     
-    public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        if let dataSources = dataSources {
+    open func numberOfSections(in tableView: UITableView) -> Int {
+        if let dataSources = self.dataSources {
             return dataSources.count
         }
         return 1
     }
     
-    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let dataSources = dataSources {
+    open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let dataSources = self.dataSources {
             let dataSource = dataSources[section]
             return dataSource.tableView(tableView, numberOfRowsInSection: section)
         }
         return 0
     }
     
-    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let dataSource = dataSources![indexPath.section]
-        return dataSource.tableView(tableView, cellForRowAtIndexPath: indexPath)
+    open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let dataSource = self.dataSources![indexPath.section]
+        return dataSource.tableView(tableView, cellForRowAt: indexPath)
     }
 
     
-    public func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        let dataSource = dataSources![indexPath.section]
-        return dataSource.tableView(tableView, canMoveRowAtIndexPath: indexPath)
+    open func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        let dataSource = self.dataSources![indexPath.section]
+        return dataSource.tableView(tableView, canMoveRowAt: indexPath)
     }
     
-    public func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        let dataSource = dataSources![indexPath.section]
-        return dataSource.tableView(tableView, canEditRowAtIndexPath: indexPath)
+    open func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        let dataSource = self.dataSources![indexPath.section]
+        return dataSource.tableView(tableView, canEditRowAt: indexPath)
     }
     
-    public func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        delegate?.commitEditingStyle?(editingStyle, forIndexPath: indexPath)
+    open func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        self.delegate?.commitEditingStyle?(editingStyle, forIndexPath: indexPath)
     }
     
-    public func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
-        if var sourceItems = dataSource(at: sourceIndexPath).items,
-           var destinationItems = dataSource(at: destinationIndexPath).items {
+    open func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        if var sourceItems = self.dataSource(at: sourceIndexPath).items,
+           var destinationItems = self.dataSource(at: destinationIndexPath).items {
             
-            let item = sourceItems.removeAtIndex(sourceIndexPath.row)
-            destinationItems.insert(item, atIndex: destinationIndexPath.row)
+            let item = sourceItems.remove(at: sourceIndexPath.row)
+            destinationItems.insert(item, at: destinationIndexPath.row)
             
-            dataSources?[sourceIndexPath.section].items = sourceItems
-            dataSources?[destinationIndexPath.section].items = destinationItems
+            self.dataSources?[sourceIndexPath.section].items = sourceItems
+            self.dataSources?[destinationIndexPath.section].items = destinationItems
         }
     }
     
-    
     //MARK - Indexing
     
-    public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    open func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         
-        if showDefaultHeaderTitle {
-            if let dataSources = dataSources {
+        if self.showDefaultHeaderTitle {
+            if let dataSources = self.dataSources {
                 let dataSource = dataSources[section]
                 return dataSource.title
             }
             return nil
         }
         
-        if !enableIndexing {
+        if !self.enableIndexing {
             return nil
         }
         
@@ -254,20 +253,20 @@ public class TableViewSectionDataSource: NSObject, UITableViewDataSource {
             return nil
         }
         
-        return UILocalizedIndexedCollation.currentCollation().sectionTitles[section]
+        return UILocalizedIndexedCollation.current().sectionTitles[section]
     }
     
-    public func sectionIndexTitles(tableView: UITableView) -> [String]? {
-        
-        if !enableIndexing {
+    open func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        if !self.enableIndexing {
             return nil
         }
         
-        return UILocalizedIndexedCollation.currentCollation().sectionTitles
+        return UILocalizedIndexedCollation.current().sectionTitles
     }
     
-    public func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int {
-        return UILocalizedIndexedCollation.currentCollation().sectionForSectionIndexTitleAtIndex(index)
+    
+    open func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return UILocalizedIndexedCollation.current().section(forSectionIndexTitle: index)
     }
 }
 

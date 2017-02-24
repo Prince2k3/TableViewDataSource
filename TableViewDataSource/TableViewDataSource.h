abf10e28b02ff0a037121c59e268f340c3bd9dea
Prince Ugwuh <pugwuh@inmotionsoftware.com>
2017-02-23 21:35:34 -0600

    Added Project for the source code...added support for carthage

---
+++ TableViewDataSource/TableViewDataSource.h
@@ -0,0 +1,19 @@
+//
+//  TableViewDataSource.h
+//  TableViewDataSource
+//
+//  Created by Prince Ugwuh on 2/23/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableViewDataSource.
+FOUNDATION_EXPORT double TableViewDataSourceVersionNumber;
+
+//! Project version string for TableViewDataSource.
+FOUNDATION_EXPORT const unsigned char TableViewDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableViewDataSource/PublicHeader.h>
+
+
